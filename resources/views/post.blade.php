9a26e1a472a0edacb24f1a4f30d6b4c950a72608
agustriyono4658 <agus.triyono4658@gmail.com>
2021-09-29 18:24:34 +0700

    Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 
 @section('container')
     <h3 class="mb-3">{{ $post->title }}</h3>
+    <p>by. agung in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
     <a href="/blog">back to posts</a>
 @endsection
