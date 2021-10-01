6cfb89b9c2263dfcae94e572a2b06fc0ff553e7c
agustriyono4658 <agus.triyono4658@gmail.com>
2021-10-01 16:54:16 +0700

    User Login & Middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -12,4 +14,27 @@ class LoginController extends Controller
             'active' => 'login'
         ]);
     }
+
+    public function authenticate(Request $request) {
+        $login = $request->validate([
+            'email' => ['required', 'email:dns'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($login)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('login-failed','Login Gagal!');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,17 +10,34 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
           @endif
+          @if (session()->has('login-failed'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('login-failed') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+          @endif
         <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-          <form>
+          <form action="/login" method="post">
+            @csrf
             <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-              <label for="floatingInput">Email address</label>
+              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
+              <label for="email">Email address</label>
+              @error('email')    
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+              @enderror
             </div>
             <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-              <label for="floatingPassword">Password</label>
+              <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+              <label for="password">Password</label>
+              @error('password')    
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+              @enderror
             </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Login</button>
           </form>
           <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register now!</a></small>
         </main>
