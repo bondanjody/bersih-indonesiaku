78947b29c443e947ca3383775b7bf8fce54f0006
bondanjody <bondanjodys@gmail.com>
2024-06-25 21:09:07 +0700

    Mengubah bahasa pada halaman login dan register.

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -16,12 +16,12 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>
           @endif
-        <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
+        <h1 class="h3 mb-3 fw-normal text-center">Form Login</h1>
           <form action="/login" method="post">
             @csrf
             <div class="form-floating">
               <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
-              <label for="email">Email address</label>
+              <label for="email">Email </label>
               @error('email')    
               <div class="invalid-feedback">
                 {{ $message }}
@@ -39,7 +39,7 @@
             </div>
             <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Login</button>
           </form>
-          <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register now!</a></small>
+          <small class="d-block text-center mt-3">Belum punya akun? <a href="/register">Daftar sekarang!</a></small>
         </main>
     </div>
 </div>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -4,12 +4,12 @@
 <div class="row justify-content-center">
     <div class="col-md-5">
         <main class="form-register">
-        <h1 class="h3 mb-3 fw-normal text-center">Register Form</h1>
+        <h1 class="h3 mb-3 fw-normal text-center">Form Registrasi</h1>
           <form action="/register" method="post">
             @csrf
             <div class="form-floating">
               <input type="text" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" placeholder="name" value="{{ old('name') }}" required>
-              <label for="name">Name</label>
+              <label for="name">Nama</label>
               @error('name')
               <div class="invalid-feedback">
                   {{ $message }}
@@ -27,7 +27,7 @@
             </div>
             <div class="form-floating">
               <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" required>
-              <label for="email">Email address</label>
+              <label for="email">Email </label>
               @error('email')
               <div class="invalid-feedback">
                   {{ $message }}
@@ -43,9 +43,9 @@
               </div>
               @enderror
             </div>
-            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Daftar</button>
           </form>
-          <small class="d-block text-center mt-3">Already registered? <a href="/login">Login now!</a></small>
+          <small class="d-block text-center mt-3">Sudah punya akun? <a href="/login">Login sekarang!</a></small>
         </main>
     </div>
 </div>
